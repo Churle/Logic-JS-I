0cb5b5a7c445375edeeeb4fa902bf54ba03e4ae6
ChurMaster <charlie_cwv@hotmail.com>
2023-10-25 10:57:59 -0300

    challenge.js adicionado

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 alert ('Bem Vindo ao jogo do numero secreto');
+let NumeroSecreto =5 ;
+let Chute = prompt ('Escolha um numero entre 1 e 10');
+if (Chute == 5) {
+    console.log('E isso ai, parabens acertou!');
+};
+if(Chute < 5){
+    console.log('Quase! o numero e maior');
+};
+if(Chute > 5){
+    console.log('Quase! o numero e menor');
+};
---
+++ challenge.js
@@ -0,0 +1,7 @@
+alert =('Boas Vindas ao nosso site')
+let nome =lua ;
+let idade =25;
+let numero_de_vendas =50;
+let saldo_indisponivel =1000;
+prompt('Erro! Preencha todoso os campos.');
+let mensagem_erro =('Erro!');
