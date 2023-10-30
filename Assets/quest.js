f63c48a2448d746efd10d16a001f076d64ffcbc1
ChurMaster <charlie_cwv@hotmail.com>
2023-10-30 00:07:35 -0300

    adding functions to the second <p>

--- Assets/quest.js
+++ Assets/quest.js
@@ -1,8 +1,32 @@
-const para = document.querySelector("p");
+// Selecionar o parágrafo com a classe "paragrafo-especial"
+var primeiroparagrafo= document.querySelector(".primeiro-paragrafo");
 
-para.addEventListener("click", atualizarNome);
+// Selecionar o parágrafo com a classe "primeiro-paragrafo"
+var primeiroParagrafo = document.querySelector(".primeiro-paragrafo");
 
-function atualizarNome() {
-  var nome = prompt("Insira um novo nome");
-  para.textContent = "Jogador 1: " + nome;
+// Adicionar ouvinte de eventos ao parágrafo "primeiro-paragrafo"
+primeiroParagrafo.addEventListener("click", function() {
+    atualizarNome(primeiroParagrafo);
+});
+
+function atualizarNome(paragrafo){
+  var nome = prompt("Qual seu nome");  
+  paragrafo.textContent = "Jogador 1: " + nome;
+}
+//---------------------------------------------------------------------------------- Segundo paragrafo.
+
+// Selecionar o primeiro parágrafo
+var segundoParagrafo = document.querySelector(".segundo-paragrafo");
+
+// Selecionar o segundo paragrafo
+var segundoParagrafo = document.querySelector(".segundo-paragrafo");
+
+// Adicionar ouvinte de eventos ao parágrafo ".segundo-paragrafo";
+segundoParagrafo.addEventListener("click", function() {
+  atualizarregiao(segundoParagrafo);
+});
+
+function atualizarregiao(paragrafo) {
+  var regiao = prompt("Insira seu estado");
+  paragrafo.textContent = "Regiao: " + regiao;
 }
