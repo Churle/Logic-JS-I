9246f9476213c14979211fe212b83fc5584a6773
ChurMaster <charlie_cwv@hotmail.com>
2023-11-03 19:08:11 -0300

    Adding else to java script

--- Assets/app.js
+++ Assets/app.js
@@ -1,12 +1,12 @@
+// inicio
 alert ('Bem Vindo ao jogo do numero secreto');
 let NumeroSecreto =5 ;
 let Chute = prompt ('Escolha um numero entre 1 e 10');
+
+// IF Chut = secret number then :
 if (Chute == 5) {
     console.log('E isso ai, parabens acertou!');
-};
-if(Chute < 5){
-    console.log('Quase! o numero e maior');
-};
-if(Chute > 5){
-    console.log('Quase! o numero e menor');
+} else {
+    alert('Voce errou!');
+
 };
