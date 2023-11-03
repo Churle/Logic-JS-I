94a204ea1028f0f2ee0637aaeb809efb89923652
ChurMaster <charlie_cwv@hotmail.com>
2023-11-03 19:54:53 -0300

    Adding else to app.js

--- Assets/app.js
+++ Assets/app.js
@@ -1,11 +1,12 @@
 // inicio
 alert ('Bem Vindo ao jogo do numero secreto');
 let NumeroSecreto =5 ;
+console.log(NumeroSecreto)
 let Chute = prompt ('Escolha um numero entre 1 e 10');
 
 // IF Chut = secret number then :
 if (Chute == 5) {
-    console.log('E isso ai, parabens acertou!');
+    console.log('E isso ai, parabens acertou o numero secreto!'+ NumeroSecreto);
 } else {
     alert('Voce errou!');
 
