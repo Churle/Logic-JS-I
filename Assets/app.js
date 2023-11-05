3e6b187b59918195890690dd6b10ebf96cc4286e
ChurMaster <charlie_cwv@hotmail.com>
2023-11-05 19:56:26 -0300

    Adding challenge 2 from quest

--- Assets/app.js
+++ Assets/app.js
@@ -3,11 +3,13 @@ alert ('Bem Vindo ao jogo do numero secreto');
 let NumeroSecreto =5 ;
 console.log(NumeroSecreto)
 let Chute = prompt ('Escolha um numero entre 1 e 10');
-
+console.log(Chute)
 // IF Chut = secret number then :
 if (Chute == 5) {
     console.log(`E isso ai, parabens acertou o numero secreto! ${NumeroSecreto}`);
+    // usando else para dar a opcao de mensagem apos o erro! 
 } else {
+    console.log('valor numero secreto '+NumeroSecreto);
     alert('Voce errou!');
-
+    alert('O numero secreto era = '+NumeroSecreto);
 };
